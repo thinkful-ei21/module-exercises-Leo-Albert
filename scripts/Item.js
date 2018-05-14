e65b655c3e2d416f84a7261ea2937e79ce312a88
Albert Sare <thesundaymailman@gmail.com>
2018-05-14 08:43:49 -0700

    finished namespacing exercise 1

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,8 +1,21 @@
 'use strict';
 
-const item = (function () {
+const Item = (function () {
+  const validateName = function(name) {
+    if(!name) throw new TypeError('Name does not exist.');
+  };
+  
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name,
+      checked: false
+    };
+  };
 
-  return {}; 
+  return {
+    validateName,
+    create
+  };
 
 }());
-
--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,19 @@
 /* global shoppingList, cuid */
 
 $(document).ready(function() {
-  shoppingList.bindEventListeners();
+
+  const itemNames = [ '', 'apples', 'pears' ];
+  itemNames.forEach(name => {
+    try {
+      Item.validateName(name);
+      store.items.push(Item.create(name));
+    } catch(error) {
+      console.log('Cannot add item: ' + error.message);
+    }
+  });
   shoppingList.render();
+
+  // shoppingList.bindEventListeners();
+  // shoppingList.render();
 });
 
